807993c040691dc9921d4027ece46ed2db3346f2
MaxenceBailly <maxence.bailly.fr@gmail.com>
2026-09-17 19:19:40 +0200

    Add holding jump and dash roll

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -2,7 +2,7 @@ extends Node
 
 var score = 0
 
-@onready var score_label = $ScoreLabel
+@onready var score_label = $%ScoreLabel
 
 func add_point():
 	score+=1
--- scripts/player.gd
+++ scripts/player.gd
@@ -6,6 +6,11 @@ const JUMP_VELOCITY = -300.0
 
 @onready var animated_sprite = $AnimatedSprite2D
 var facing_direction = false
+var is_rolling : bool = false
+var can_roll : bool = true
+@onready var dash_timer = $DashTimer
+@onready var dash_cooldown_timer = $DashCooldownTimer
+var panda_transformation : bool = true
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -13,11 +18,15 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
+	if Input.is_action_just_pressed("dash_roll") and can_roll:
+		dash_timer.start()
+		dash_cooldown_timer.start()
+		is_rolling = true
+		can_roll = false
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
@@ -37,6 +46,21 @@ func _physics_process(delta: float) -> void:
 	else:
 		animated_sprite.play("jump")
 		
+	if is_rolling:
+		print("rolling")
+		animated_sprite.play("dash_roll")
+		
+	if is_rolling:
+		velocity.x = (-1 if facing_direction else 1) * SPEED*2
+		
 	animated_sprite.flip_h = facing_direction
 
 	move_and_slide()
+
+
+func _on_dash_timer_timeout() -> void:
+	is_rolling = false
+
+
+func _on_dash_cooldown_timer_timeout() -> void:
+	can_roll = true
